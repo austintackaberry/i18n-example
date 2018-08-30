773197df78625670ab30068dde7c4445bc435f87
Dónal Murtagh <donal.murtagh@lookinglocal.gov.uk>
2018-08-30 16:20:53 +0100

    Add an example of defining a translatable message via the JavaScript API

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import {FormattedMessage} from 'react-intl';
+import {injectIntl, defineMessages} from 'react-intl';
 
-function Weather() {
-  return (
-    <p>
-      <FormattedMessage id="Weather.message" defaultMessage="Because it is sunny!" />
-    </p>
-  );
+const allMessages = defineMessages({
+    addressTitle: {id: 'field.address.title', defaultMessage: "Home address"}
+});
+
+function Weather(props) {
+
+    // helper function for getting a message translation
+    const msg = key => props.intl.formatMessage(allMessages[key]);
+
+    return (
+        <React.Fragment>
+            <h3>{msg('addressTitle')}</h3>
+            <p>
+                <FormattedMessage id="Weather.message" defaultMessage="Because it is sunny!"/>
+            </p>
+        </React.Fragment>
+    );
 }
 
-export default Weather;
+export default injectIntl(Weather);
